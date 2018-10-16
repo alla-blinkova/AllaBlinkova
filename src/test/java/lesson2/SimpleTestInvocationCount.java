6e413d555028b3315c07acc2310e50dae00e144f
alla-blinkova <blinkaa@mail.ru>
2018-10-16 12:10:22 +0300

    Add lesson2

---
+++ src/test/java/lesson2/SimpleTestInvocationCount.java
@@ -0,0 +1,44 @@
+package lesson2;
+
+import base.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+
+public class SimpleTestInvocationCount extends TestBase {
+
+    @Test(invocationCount = 3, threadPoolSize = 3)
+    public void simpleTest() {
+
+        //Open BR
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        //2 Navigate
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+
+        //3 Assert Title
+        assertEquals(driver.getTitle(), "Home Page");
+
+        //4 Login
+        driver.findElement(By.cssSelector(".profile-photo")).click();
+        driver.findElement(By.cssSelector("[id = 'Name']")).sendKeys("epam");
+        driver.findElement(By.cssSelector("[id = 'Password']")).sendKeys("1234");
+        driver.findElement(By.cssSelector(".login [type = 'submit']")).click();
+
+        WebElement mainTitle = driver.findElement(By.cssSelector("h3.main-title"));
+        assertEquals(mainTitle.getText(), "EPAM FRAMEWORK WISHES…");
+
+        //5 Close BR
+        driver.close();
+    }
+}
